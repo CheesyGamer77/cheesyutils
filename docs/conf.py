49b2c95f4a1b6aecbd3781326e2b3ff7b5829fe3
Mineinjava <fbisurveillancevan525178@gmail.com>
2021-05-07 09:05:49 -0700

    update path

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 version = ''
-with open('../discord/__init__.py') as f:
+with open('../src/cheesyutils/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 # The full version, including alpha/beta/rc tags.
